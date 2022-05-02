be7cd146972545ef71a3fdec589bf132a47741b5
Alexandr Merkulov <sashrabin@gmail.com>
2022-05-02 15:58:47 +0300

    hometask 2
    - Footer component changed from functional to class
    - Added state in Footer, after click on the link to original design, will render thank you sign

--- src/pages/Main/components/Footer/Footer.jsx
+++ src/pages/Main/components/Footer/Footer.jsx
@@ -1,29 +1,47 @@
 import React from 'react';
 
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <span className="footer__author_name">Меркулов Саша</span>
-      <span className="footer__author_github">
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/SashkoRabin"
+class Footer extends React.Component {
+  constructor() {
+    super();
+    this.state = { isOriginalDesignLooked: false };
+    this.originalDesignLooked = this.originalDesignLooked.bind(this);
+  }
+
+  originalDesignLooked() {
+    this.setState({
+      isOriginalDesignLooked: true,
+    });
+  }
+
+  render() {
+    return (
+      <footer className="footer">
+        <span className="footer__author_name">Меркулов Саша</span>
+        <span className="footer__author_github">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/SashkoRabin"
+          >
+            GitHub
+          </a>
+        </span>
+        <span
+          onClick={this.originalDesignLooked}
+          className="footer__design_link"
         >
-          GitHub
-        </a>
-      </span>
-      <span className="footer__design_link">
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://nicepage.com/ru/st/19813/vkusnaya-i-svezhaya-eda-shablon-sayta"
-        >
-          Original design...
-        </a>
-      </span>
-    </footer>
-  );
-};
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://nicepage.com/ru/st/19813/vkusnaya-i-svezhaya-eda-shablon-sayta"
+          >
+            Original design...
+          </a>
+        </span>
+        {this.state.isOriginalDesignLooked && <span>Спасибо!!!</span>}
+      </footer>
+    );
+  }
+}
 
 export default Footer;
