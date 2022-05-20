f3affd61484f8556b4b37e56434a1b0f1c411336
Alexandr Merkulov <sashrabin@gmail.com>
2022-05-20 10:38:18 +0300

    Issue #3 fixed
    - new Timer logic

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -7,25 +7,20 @@ export default class Timer extends Component {
       hours: 0,
       minutes: 0,
       seconds: 0,
+      start: +new Date() / 1000,
+      now: +new Date() / 1000,
     },
   };
   componentDidMount = () => {
     this.timer = setInterval(() => {
-      if (this.state.time.seconds < 60) {
-        this.setState((prev) => ({
-          time: { ...prev.time, seconds: prev.time.seconds + 1 },
-        }));
-      }
-      if (this.state.time.seconds >= 60) {
-        this.setState((prev) => ({
-          time: { ...prev.time, minutes: prev.time.minutes + 1, seconds: 0 },
-        }));
-      }
-      if (this.state.time.minutes >= 60) {
-        this.setState((prev) => ({
-          time: { ...prev.time, minutes: 0, hours: prev.time.hours + 1 },
-        }));
-      }
+      this.setState((prev) => (prev.time.now = +new Date() / 1000));
+      this.setState((prev) => {
+        prev.time.seconds = Math.trunc(prev.time.now - prev.time.start);
+        prev.time.minutes = Math.trunc(prev.time.seconds / 60);
+        prev.time.hours = Math.trunc(prev.time.minutes / 60);
+        prev.time.seconds = prev.time.seconds % 60;
+        prev.time.minutes = prev.time.minutes % 60;
+      });
     }, 1000);
   };
   componentWillUnmount = () => {
